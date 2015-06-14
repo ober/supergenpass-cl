c3bf59861ef2b55b30e6d4f9185ddf8b3bf249c3
Jaime Fournier <jaimef@linbsd.org>
2015-06-14 05:23:51 -0700

    Add capi prompt

--- supergenpass.lisp
+++ supergenpass.lisp
@@ -1,6 +1,11 @@
 (ql:quickload '(:cl-base64 :md5 :cl-ppcre))
 
 
+
+#+lispworks
+(defun prompt-for-input (query)
+  (capi:prompt-for-string (format nil "~A" query) :overwrite-character #\$))
+
 (defun b64-md5 (pickle)
   "Encrypt the string given to us as Base64 encoded Md5 byte stream"
   (substitute #\9 #\+
@@ -13,10 +18,9 @@
 (defun sgp-generate (password domain length)
   "Create a unique password for a given domain and master password"
   (let* ((i 0)
-	(initial (format nil "~A:~A" password domain))
-        (results (b64-md5 initial)))
+	 (initial (format nil "~A:~A" password domain))
+	 (results (b64-md5 initial)))
     (loop
-       ;;(format t "inloop: i:~A results:~A~%" i results)
        (if (and
 	    (> i 8)
 	    (secure-enough results length))
@@ -31,3 +35,10 @@
    (ppcre:all-matches "[0-9]" (subseq results 0 len))
    (ppcre:all-matches "[A-Z]" (subseq results 0 len))
    (ppcre:all-matches "^[a-z]" (subseq results 0 len))))
+
+#+lispworks
+(defun main ()
+  "Primary entry point"
+  (let ((master (prompt-for-input "Master:"))
+	(domain (prompt-for-input "Domain:")))
+    (sgp-generate master domain 10)))
