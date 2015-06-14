7e4ae6c315fff9f6b68b2f369a68f225edf1921c
Jaime Fournier <jaimef@linbsd.org>
2015-06-14 03:42:31 -0700

    initial import

---
+++ supergenpass.lisp
@@ -0,0 +1,35 @@
+(ql:quickload '(:cl-base64 :md5 :cl-ppcre))
+
+
+(defun b64-md5 (pickle)
+  "Encrypt the string given to us as Base64 encoded Md5 byte stream"
+  (substitute #\9 #\+
+	      (substitute #\A #\=
+			  (substitute #\8 #\/
+				      (base64:usb8-array-to-base64-string
+				       (md5:md5sum-string pickle))))))
+
+
+(defun sgp-generate (password domain length)
+  "Create a unique password for a given domain and master password"
+  (let ((i 0)
+	(initial (format nil "~A:~A" password domain)))
+    (defparameter results (b64-md5 initial))
+    (loop
+     (format t "INLOOP i:~A results:~A~%" i results)
+       (if (and
+	    (> i 9)
+	    (secure-enough results length))
+	   (return (subseq results 0 length)))
+       (defparameter results (b64-md5 results))
+       (defparameter i (1+ i)))))
+
+
+
+(defun secure-enough (results len)
+  "Ensure the password we have is sufficiently secure"
+  (and
+   (> (length results) len)
+   (ppcre:all-matches "[0-9]" (subseq results 0 len))
+   (ppcre:all-matches "[A-Z]" (subseq results 0 len))
+   (ppcre:all-matches "^[a-z]" (subseq results 0 len))))
